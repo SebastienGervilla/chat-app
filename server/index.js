9c8327eb337e87c868edd6ecffacc4cbf710ef10
Sébastien Gervilla <92156546+SebastienGervilla@users.noreply.github.com>
2022-05-27 13:07:57 +0200

    Updated

--- server/index.js
+++ server/index.js
@@ -8,14 +8,6 @@ const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname + "/../client")));
 
-// const { Server } = require('socket.io')
-// const io = new Server(server, {
-//     cors: {
-//         origin: "http://localhost:3000",
-//         methods: ["GET", "POST"]
-//     }
-// })
-
 io.on('connection', (socket) => {
     console.log(socket.id);
 
